e9c6bb5c14a8665bfbfaa613ab5ff4684d43c87b
vipultrimbake610 <vipultrimbake610@gmail.com>
2024-05-26 18:52:01 +0530

    Now user will recieve a confimation email on his booking

--- src/main/java/com/acciojob/BookMyShow/Project/Services/TicketService.java
+++ src/main/java/com/acciojob/BookMyShow/Project/Services/TicketService.java
@@ -12,6 +12,8 @@ import com.acciojob.BookMyShow.Project.models.ShowSeat;
 import com.acciojob.BookMyShow.Project.models.Ticket;
 import com.acciojob.BookMyShow.Project.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,6 +23,9 @@ public class TicketService {
 
     @Autowired
     private ShowRepository showRepository;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     @Autowired
     private ShowSeatRepository showSeatRepository;
@@ -44,11 +49,15 @@ public class TicketService {
         for (ShowSeat showSeat : showSeatList){
             String seatNo  = showSeat.getSeatNo();
             if(bookTicketRequest.getRequestedSeats().contains(seatNo)){
-                showSeat.setIsBooked(Boolean.TRUE);
-                if(showSeat.getSeatType().equals(SeatType.CLASSIC)){
-                    totalAmount += 100;
+                if(showSeat.getIsBooked() ==  Boolean.TRUE){
+                    return "Requested seat seats are already booked!";
                 }else{
-                    totalAmount += 150;
+                    showSeat.setIsBooked(Boolean.TRUE);
+                }
+                if(showSeat.getSeatType().equals(SeatType.CLASSIC)){
+                    totalAmount += 200;
+                }else{
+                    totalAmount += 350;
                 }
             }
         }
@@ -71,7 +80,10 @@ public class TicketService {
     }
 
     public TicketResponse generateTicket(String ticketId){
+
         Ticket ticket = ticketRepository.findById(ticketId).get();
+        User user = ticket.getUser();
+        System.out.println(user);
 
         TicketResponse ticketResponse = TicketResponse.builder()
                 .bookedSeats(ticket.getBookedSeats())
@@ -82,6 +94,32 @@ public class TicketService {
                 .totalAmount(ticket.getTotalAmount())
                 .build();
 
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(user.getEmailId());
+        mailMessage.setFrom("springacciojob@gmail.com");
+        mailMessage.setSubject("Booking Confirmation: Your Seats for "+ticketResponse.getMovieName()+" on "+ticketResponse.getShowDate()+" from BookMyShow");
+
+        String body = "Dear "+user.getName()+",\n" +
+                "\n" +
+                "We are thrilled to confirm that your seats have been successfully booked for the respected show at "+ticketResponse.getTheaterName()+" on "+ticketResponse.getShowDate()+" at "+ticketResponse.getShowTime()+".\n" +
+                "\n" +
+                "Booking Details:\n" +
+                "\n" +
+                "Show: "+ticketResponse.getMovieName()+"\n" +
+                "Venue: "+ticketResponse.getTheaterName()+"\n" +
+                "Date: "+ticketResponse.getShowDate()+"\n" +
+                "Time: "+ticketResponse.getShowTime()+"\n" +
+                "Seat(s): "+ticket.getBookedSeats()+"\n" +
+                "\n" +
+                "Should you have any further inquiries or require assistance, please do not hesitate to contact us.\n" +
+                "\n" +
+                "Warm regards\n" +
+                "BookMyShwo\n" +
+                "\n";
+
+        mailMessage.setText(body);
+
+        javaMailSender.send(mailMessage);
         return ticketResponse;
     }
 }
